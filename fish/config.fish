7c7197cd43d12bd0e32f6a8ddf96840ef813465e
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2024-01-15 10:38:01 +0530

    Add easy directory and file navigation using fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,21 @@
 set fish_greeting ""
 
-alias ls="ls -a"
+alias ls="exa -a"
 alias vim="nvim"
 alias ll="exa -la"
+alias sd="cd ~ && cd (find * .* -type d -o -type l | fzf)"
+
+function sf
+    set selected_file (find . -type f | fzf)
+    if test -n "$selected_file"
+        nvim "$selected_file"
+    end
+end
 
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/Stuff/Odin
+fish_add_path ~/scripts
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
