41df181325d9f328acc8984db8c98a7afa6af64d
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2024-01-21 12:03:00 +0530

    Minor tweaks

---
+++ rofi/powermenu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+options="Shutdown\nRestart\nSuspend"
+
+choice=$(echo -e "$options" | rofi -dmenu -markup-rows -i -p "Power Menu:")
+
+case $choice in
+    "Shutdown")
+        systemctl poweroff
+        ;;
+    "Restart")
+        systemctl reboot
+        ;;
+    "Suspend")
+        systemctl suspend
+        ;;
+esac
+
